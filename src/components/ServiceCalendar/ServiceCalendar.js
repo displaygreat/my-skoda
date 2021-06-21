a1930d8a1f39766faafcc27795a69702ed7a443d
displaygreat <displaygreat@gmail.com>
2021-06-21 12:16:17 +0300

    refactoring to hooks component ServiceCalendar

--- src/components/ServiceCalendar/ServiceCalendar.js
+++ src/components/ServiceCalendar/ServiceCalendar.js
@@ -1,181 +1,158 @@
 import "./ServiceCalendar.css";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import moment from "moment";
+import { useData } from "../../shared/dataContext";
 
 const ServiceCalendar = () => {
-  // const activeUser = useContext(UserContext);
-  // const vehicle = useContext(VehicleContext);
-  // console.log(activeUser);
-  // console.log(vehicle);
+  const { setValues, data } = useData();
 
-  // const [inspectionBeforeTest, setInspectionBeforeTest] = useState("");
-  // const [multiPointInspection, setMultiPointInspection] = useState("");
-  // const [summerInspection, setSummerInspection] = useState("");
-  // const [winterInspection, setWinterInspection] = useState("");
+  const [inspectionBeforeTest, setInspectionBeforeTest] = useState("");
+  const [multiPointInspection, setMultiPointInspection] = useState("");
+  const [summerInspection, setSummerInspection] = useState("");
+  const [winterInspection, setWinterInspection] = useState("");
 
-  // let carTest = moment(vehicle.mivchan_acharon_dt).format("DD/MM/YYYY");
-  // let carYear = vehicle.shnat_yitzur;
-  // let lastInspection;
-  // if (!activeUser || activeUser === "undefind") {
-  //   window.location = "#/login";
-  // } else {
-  //   lastInspection = activeUser.lastInspection;
-  //   console.log(lastInspection);
-  // }
-  // // constructor(props) {
-  // //   super(props);
-  // //   this.state = {
-  // //     inspectionBeforeTest: "",
-  // //     multiPointInspection: "",
-  // //     summerInspection: "",
-  // //     winterInspection: "",
-  // //   };
-  // // }
+  let carTest = moment(data.mivchan_acharon_dt).format("DD/MM/YYYY");
+  let carYear = data.shnat_yitzur;
+  let lastInspection = data.lastInspection;
 
-  // useEffect(() => {
-  //   getInspectionBeforeTest();
-  //   getMultiPointInspection();
-  //   getSeasonInspection();
-  // }, []);
+  useEffect(() => {
+    getInspectionBeforeTest();
+    getMultiPointInspection();
+    getSeasonInspection();
+  }, []);
 
-  // // componentDidUpdate(prevProps, prevState) {
-  // //   if (this.props !== prevProps) {
-  // //     this.getInspectionBeforeTest();
-  // //   }
-  // // }
+  const getFullDate = (date) => {
+    let momentObjDate = moment(date, "DD/MM/YYYY");
+    let fullDate = momentObjDate.toDate();
+    return fullDate;
+  };
 
-  // const getFullDate = (date) => {
-  //   let momentObjDate = moment(date, "DD/MM/YYYY");
-  //   let fullDate = momentObjDate.toDate();
-  //   return fullDate;
-  // };
+  const getInspectionBeforeTest = () => {
+    let yearNow = new Date().getFullYear();
+    let year = carYear;
+    let carAge = yearNow - year;
 
-  // const getInspectionBeforeTest = () => {
-  //   let yearNow = new Date().getFullYear();
-  //   let year = carYear;
-  //   let carAge = yearNow - year;
+    let test = carTest;
+    let fullDateLastTest = getFullDate(test);
 
-  //   let test = carTest;
-  //   let fullDateLastTest = getFullDate(test);
+    let momentObjNextTest = {};
+    if (carAge <= 2) {
+      momentObjNextTest = moment(fullDateLastTest).add(3, "years");
+    }
+    if (carAge >= 3 && carAge < 20) {
+      momentObjNextTest = moment(fullDateLastTest).add(1, "years");
+    }
+    if (carAge >= 20) {
+      momentObjNextTest = moment(fullDateLastTest).add(6, "months");
+    }
+    let momentObjDate = moment(momentObjNextTest).subtract(14, "days");
+    let date = moment(momentObjDate).format("DD/MM/YYYY");
+    setInspectionBeforeTest(date);
+  };
 
-  //   let momentObjNextTest = {};
-  //   if (carAge <= 2) {
-  //     momentObjNextTest = moment(fullDateLastTest).add(3, "years");
-  //   }
-  //   if (carAge >= 3 && carAge < 20) {
-  //     momentObjNextTest = moment(fullDateLastTest).add(1, "years");
-  //   }
-  //   if (carAge >= 20) {
-  //     momentObjNextTest = moment(fullDateLastTest).add(6, "months");
-  //   }
-  //   let momentObjDate = moment(momentObjNextTest).subtract(14, "days");
-  //   let date = moment(momentObjDate).format("DD/MM/YYYY");
-  //   setInspectionBeforeTest(date);
-  // };
+  const getMultiPointInspection = () => {
+    let inspection = lastInspection;
+    if (inspection === "" || inspection === undefined) {
+      let dateNow = new Date();
+      let momentObjDate = moment(dateNow).add(7, "days");
+      let date = moment(momentObjDate).format("DD/MM/YYYY");
+      setMultiPointInspection(date);
+      return;
+    }
+    let fullDate = getFullDate(inspection);
+    let momentObjDate = moment(fullDate).add(1, "year");
+    let date = moment(momentObjDate).format("DD/MM/YYYY");
+    setMultiPointInspection(date);
+  };
 
-  // const getMultiPointInspection = () => {
-  //   let inspection = lastInspection;
-  //   if (inspection === "" || inspection === undefined) {
-  //     let dateNow = new Date();
-  //     let momentObjDate = moment(dateNow).add(7, "days");
-  //     let date = moment(momentObjDate).format("DD/MM/YYYY");
-  //     setMultiPointInspection(date);
-  //     return;
-  //   }
-  //   let fullDate = getFullDate(inspection);
-  //   let momentObjDate = moment(fullDate).add(1, "year");
-  //   let date = moment(momentObjDate).format("DD/MM/YYYY");
-  //   setMultiPointInspection(date);
-  // };
+  const getSeasonInspection = () => {
+    let fullDateNow = new Date();
+    let dateNow = moment(fullDateNow);
+    let momentObjDateAfterYear = moment(fullDateNow).add(1, "year");
+    let dateAfterYear = moment(momentObjDateAfterYear);
+    let firstOfMay;
+    let firstOfNovember;
+    while (dateAfterYear > dateNow) {
+      if (dateNow.format("DD/MM") === "01/05") {
+        firstOfMay = dateNow.format("DD/MM/YYYY");
+      }
+      if (dateNow.format("DD/MM") === "01/11") {
+        firstOfNovember = dateNow.format("DD/MM/YYYY");
+      }
+      dateNow.add(1, "day");
+    }
+    setSummerInspection(firstOfMay);
+    setWinterInspection(firstOfNovember);
+  };
 
-  // const getSeasonInspection = () => {
-  //   let fullDateNow = new Date();
-  //   let dateNow = moment(fullDateNow);
-  //   let momentObjDateAfterYear = moment(fullDateNow).add(1, "year");
-  //   let dateAfterYear = moment(momentObjDateAfterYear);
-  //   let firstOfMay;
-  //   let firstOfNovember;
-  //   while (dateAfterYear > dateNow) {
-  //     if (dateNow.format("DD/MM") === "01/05") {
-  //       firstOfMay = dateNow.format("DD/MM/YYYY");
-  //     }
-  //     if (dateNow.format("DD/MM") === "01/11") {
-  //       firstOfNovember = dateNow.format("DD/MM/YYYY");
-  //     }
-  //     dateNow.add(1, "day");
-  //   }
-  //   setSummerInspection(firstOfMay);
-  //   setWinterInspection(firstOfNovember);
-  // };
+  const scheduleServices = (date) => {
+    let fullRecommendedDate = getFullDate(date);
+    let momentObjDateToShedule = moment(fullRecommendedDate).subtract(
+      7,
+      "days"
+    );
+    let dateToShedule = moment(momentObjDateToShedule).format("DD/MM/YYYY");
+    return dateToShedule;
+  };
 
-  // const scheduleServices = (date) => {
-  //   let fullRecommendedDate = this.getFullDate(date);
-  //   let momentObjDateToShedule = moment(fullRecommendedDate).subtract(
-  //     7,
-  //     "days"
-  //   );
-  //   let dateToShedule = moment(momentObjDateToShedule).format("DD/MM/YYYY");
-  //   return dateToShedule;
-  // };
+  const getSortedData = () => {
+    const services = [
+      {
+        recommended: inspectionBeforeTest,
+        service: "Inspection Before Annual Vehicle Licensing Test",
+        shedule: scheduleServices(inspectionBeforeTest),
+      },
+      {
+        recommended: multiPointInspection,
+        service: "Multi-Point Inspection",
+        shedule: scheduleServices(multiPointInspection),
+      },
+      {
+        recommended: winterInspection,
+        service: "Inspection before winter",
+        shedule: scheduleServices(winterInspection),
+      },
+      {
+        recommended: summerInspection,
+        service: "Inspection before summer",
+        shedule: scheduleServices(summerInspection),
+      },
+    ];
+    const sortedData = services.sort((a, b) => {
+      let dateA = new Date(a.recommended.split("/").reverse().join("-"));
+      let dateB = new Date(b.recommended.split("/").reverse().join("-"));
+      return dateA - dateB;
+    });
+    return sortedData;
+  };
 
-  // const getSortedData = () => {
-  //   const services = [
-  //     {
-  //       recommended: inspectionBeforeTest,
-  //       service: "Inspection Before Annual Vehicle Licensing Test",
-  //       shedule: scheduleServices(inspectionBeforeTest),
-  //     },
-  //     {
-  //       recommended: multiPointInspection,
-  //       service: "Multi-Point Inspection",
-  //       shedule: scheduleServices(multiPointInspection),
-  //     },
-  //     {
-  //       recommended: winterInspection,
-  //       service: "Inspection before winter",
-  //       shedule: scheduleServices(winterInspection),
-  //     },
-  //     {
-  //       recommended: summerInspection,
-  //       service: "Inspection before summer",
-  //       shedule: scheduleServices(summerInspection),
-  //     },
-  //   ];
-  //   const sortedData = services.sort((a, b) => {
-  //     let dateA = new Date(a.recommended.split("/").reverse().join("-"));
-  //     let dateB = new Date(b.recommended.split("/").reverse().join("-"));
-  //     return dateA - dateB;
-  //   });
-  //   return sortedData;
-  // };
-
-  // const sortedData = getSortedData();
-  // const servicesTable = sortedData.map((service, index) => {
-  //   return (
-  //     <tbody key={index}>
-  //       <tr>
-  //         <th className="service-cell">
-  //           <small>recommended</small>
-  //           <br />
-  //           {service.recommended}
-  //         </th>
-  //         <td className="service-cell">{service.service}</td>
-  //         <td className="service-cell">
-  //           <small>schedule</small>
-  //           <br />
-  //           <a className="schedule-link" href="#/schedule">
-  //             {service.shedule}
-  //           </a>
-  //         </td>
-  //       </tr>
-  //     </tbody>
-  //   );
-  // });
+  const sortedData = getSortedData();
+  const servicesTable = sortedData.map((service, index) => {
+    return (
+      <tbody key={index}>
+        <tr>
+          <th className="service-cell">
+            <small>recommended</small>
+            <br />
+            {service.recommended}
+          </th>
+          <td className="service-cell">{service.service}</td>
+          <td className="service-cell">
+            <small>schedule</small>
+            <br />
+            <a className="schedule-link" href="#/schedule">
+              {service.shedule}
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    );
+  });
 
   return (
     <div className="c-service-calendar col-md-12 col-lg-4">
       <h1 className="display-4 my-skoda-title">Service Calendar</h1>
-      {/* <p className="text-regular">
+      <p className="text-regular">
         Keep Your Skoda At Its Best with Our Service
       </p>
       <span className="text-small">
@@ -192,7 +169,7 @@ const ServiceCalendar = () => {
           </tr>
         </thead>
         {servicesTable}
-      </table> */}
+      </table>
     </div>
   );
 };
